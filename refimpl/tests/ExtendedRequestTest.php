516118a3d7645341d3d4ba56799b61f6dead360c
Paul M. Jones <pmjones88@gmail.com>
2016-09-21 13:13:13 -0500

    add test for extending StdRequest

---
+++ refimpl/tests/ExtendedRequestTest.php
@@ -0,0 +1,71 @@
+<?php
+// this allows for using this test for *both* the reference
+// implementation *and* the extension
+if (! class_exists('StdRequest')) {
+    require dirname(__DIR__) . '/src/StdRequest.php';
+}
+
+/**
+ *
+ * Notes:
+ *
+ * - Exceptions should use the class name, not the literal StdRequest name.
+ *
+ */
+class ExtendedRequest extends StdRequest
+{
+    protected $other;
+
+    public function __get($key)
+    {
+        if (property_exists($this, $key)) {
+            return $this->$key;
+        }
+
+        return parent::$key;
+    }
+
+    public function setParent($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function setOther($value)
+    {
+        $this->other = $value;
+    }
+}
+
+class ExtendedRequestTest extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        $_SERVER['HTTP_HOST'] = 'example.com';
+    }
+
+    public function test__construct()
+    {
+        $request = new ExtendedRequest();
+        $this->assertInstanceOf(ExtendedRequest::CLASS, $request);
+    }
+
+    public function testParentPropertiesStayReadOnly()
+    {
+        $request = new ExtendedRequest();
+
+        $this->setExpectedException(
+            RuntimeException::CLASS,
+            'StdRequest is read-only.'
+        );
+
+        $request->setParent('method', 'PATCH');
+    }
+
+    public function testChildPropertiesAreUnderChildControl()
+    {
+        $request = new ExtendedRequest();
+        $request->setOther('foo');
+        $this->assertSame('foo', $request->other);
+        $this->assertSame('example.com', $request->url['host']);
+    }
+}
