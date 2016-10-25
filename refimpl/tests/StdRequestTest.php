a6c01056f07161b8d779308dc364986e58dcf4d6
Paul M. Jones <pmjones88@gmail.com>
2016-10-25 15:33:26 -0500

    add $_initialized property, and disallow __get()/__isset() on underscore-prefixed properties

--- refimpl/tests/StdRequestTest.php
+++ refimpl/tests/StdRequestTest.php
@@ -69,6 +69,16 @@ class StdRequestTest extends PHPUnit_Framework_TestCase
         $request->noSuchProperty;
     }
 
+    public function test__getUnderscorePrefixedProperty()
+    {
+        $request = new StdRequest();
+        $this->setExpectedException(
+            RuntimeException::CLASS,
+            'StdRequest::$_initialized does not exist.'
+        );
+        $request->_initialized;
+    }
+
     public function test__unset()
     {
         $_SERVER['REQUEST_METHOD'] = 'PATCH';
@@ -89,6 +99,7 @@ class StdRequestTest extends PHPUnit_Framework_TestCase
         $request = new StdRequest();
         $this->assertTrue(isset($request->method));
         $this->assertFalse(isset($request->noSuchProperty));
+        $this->assertFalse(isset($request->_initialized));
     }
 
     public function test__set()
