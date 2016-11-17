d5b121e83f3ba0d74dbfbc45cb522de67c5bdd38
Paul M. Jones <pmjones88@gmail.com>
2016-11-17 15:04:30 -0600

    update release date and srouce code version

--- php_request.h
+++ php_request.h
@@ -3,7 +3,7 @@
 #define PHP_REQUEST_H
 
 #define PHP_REQUEST_NAME "request"
-#define PHP_REQUEST_VERSION "0.0.0"
+#define PHP_REQUEST_VERSION "1.0.0a1"
 
 extern zend_module_entry request_module_entry;
 #define phpext_request_ptr &request_module_entry
