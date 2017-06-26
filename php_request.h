acaa9671bf73c609f65f42c12c50780d490d5e08
Remi Collet <remi@remirepo.net>
2017-06-26 16:50:24 +0200

    fix for PHP 7.2

--- php_request.h
+++ php_request.h
@@ -8,6 +8,21 @@
 extern zend_module_entry request_module_entry;
 #define phpext_request_ptr &request_module_entry
 
+#if PHP_VERSION_ID >= 70200
+
+#define REQUEST_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX   ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX
+#define REQUEST_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX    ZEND_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX
+
+#else
+
+#define REQUEST_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, type,       allow_null) \
+           ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, type, NULL, allow_null)
+
+#define REQUEST_BEGIN_ARG_WITH_RETURN_OBJ_INFO_EX(name, return_reference, required_num_args,            classname, allow_null) \
+          ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(name, return_reference, required_num_args, IS_OBJECT, classname, allow_null)
+
+#endif
+
 #endif	/* PHP_REQUEST_H */
 
 /*
