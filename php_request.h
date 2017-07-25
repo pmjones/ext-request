225ddc7ae3d98094d7776734528ec87f12a4953b
Paul M. Jones <pmjones88@gmail.com>
2017-07-25 12:41:26 -0500

    update the version number in code

--- php_request.h
+++ php_request.h
@@ -3,7 +3,7 @@
 #define PHP_REQUEST_H
 
 #define PHP_REQUEST_NAME "request"
-#define PHP_REQUEST_VERSION "1.0.0b1"
+#define PHP_REQUEST_VERSION "1.0.0b2"
 
 extern zend_module_entry request_module_entry;
 #define phpext_request_ptr &request_module_entry
