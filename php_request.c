3d7fd1ca9e3e80792d73326b635423cde29cc245
John Boehr <john@johnboehr.is>
2020-02-07 15:49:14 -0800

    Fix double-free in setCookie
    Removing the block should be safe because the strings are all
    added to the return zval

--- php_request.c
+++ php_request.c
@@ -1565,18 +1565,6 @@ static void server_response_set_cookie(INTERNAL_FUNCTION_PARAMETERS, zend_bool u
     // Cleanup
     zval_ptr_dtor(&member);
 
-    if (expires_or_options && Z_TYPE_P(expires_or_options) == IS_ARRAY) {
-        if (path) {
-            zend_string_release(path);
-        }
-        if (domain) {
-            zend_string_release(domain);
-        }
-        if (samesite) {
-            zend_string_release(samesite);
-        }
-    }
-
     RETURN_TRUE;
 }
 
